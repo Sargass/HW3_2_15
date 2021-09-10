01bb7605b34d29255adf380e247393ab19b9993c
Sargass <alexey.simochkin@gmail.com>
2021-09-10 17:11:56 +0300

    Initial Commit

---
+++ HW3_2_15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW3_2_15
+//
+//  Created by Алексей on 10.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
