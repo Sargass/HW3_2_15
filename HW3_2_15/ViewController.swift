da00470ac34df5b8ff42fa42b0a9a25c2637ae70
Sargass <alexey.simochkin@gmail.com>
2021-09-15 22:24:22 +0300

    BackgroundColor

--- HW3_2_15/ViewController.swift
+++ HW3_2_15/ViewController.swift
@@ -9,19 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Labels
     @IBOutlet weak var RedLabel: UILabel!
     @IBOutlet weak var BlueLabel: UILabel!
     @IBOutlet weak var GreenLabel: UILabel!
     
+    // Sliders
     @IBOutlet weak var RedSlider: UISlider!
     @IBOutlet weak var BlueSlider: UISlider!
     @IBOutlet weak var GreenSlider: UISlider!
     
+    // View
+    @IBOutlet weak var ViewColor: UIView!
+    
+    // Variables
+    private var currentRed: CGFloat  = 0
+    private var currentBlue: CGFloat = 0
+    private var currentGreen: CGFloat = 0
+    private var currrentColorView: CGFloat = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ViewColor.layer.cornerRadius = 20
+      //  ViewColor.layer.backgroundColor = 0
+        
     }
-
+    
+//    func colorFromSliders() -> UIColor? {
+    
+ //   }
 
 }
 
