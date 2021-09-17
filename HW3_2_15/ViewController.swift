eab67343e3814728ac3da8fe412eed8d94168bfd
Sargass <alexey.simochkin@gmail.com>
2021-09-17 14:26:04 +0300

    Refactoring code

--- HW3_2_15/ViewController.swift
+++ HW3_2_15/ViewController.swift
@@ -10,77 +10,37 @@ import UIKit
 class ViewController: UIViewController {
 
     // Labels
-    @IBOutlet weak var RedLabel: UILabel!
-    @IBOutlet weak var BlueLabel: UILabel!
-    @IBOutlet weak var GreenLabel: UILabel!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
     
     // Sliders
-    @IBOutlet weak var RedSlider: UISlider!
-    @IBOutlet weak var BlueSlider: UISlider!
-    @IBOutlet weak var GreenSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
     
     // View
-    @IBOutlet weak var ViewColor: UIView!
-    
-    // Variables
-    private var currentRed: CGFloat  = 0
-    private var currentBlue: CGFloat = 0
-    private var currentGreen: CGFloat = 0
-    private var currrentColorView: CGFloat = 0
-    
+    @IBOutlet weak var viewColor: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewCurrent()
         
-        redSliderView()
-        blueSliderView()
-        greenSliderView()
+        viewColor.layer.cornerRadius = 20
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        
+        
+        
+    }
 
-    }
-    
-    func redSliderView() {
-        RedSlider.value = 125
-        RedSlider.minimumValue = 0
-        RedSlider.maximumValue = 255
-        RedSlider.minimumTrackTintColor = .red
-    }
-    
-    func blueSliderView() {
-        BlueSlider.value = 125
-        BlueSlider.minimumValue = 0
-        BlueSlider.maximumValue = 255
-        BlueSlider.minimumTrackTintColor = .blue
-    }
-    
-    func greenSliderView(){
-        GreenSlider.value = 125
-        GreenSlider.minimumValue = 0
-        GreenSlider.maximumValue = 255
-        GreenSlider.minimumTrackTintColor = .green
-    }
-    
     func viewCurrent(){
-        ViewColor.layer.cornerRadius = 20
-        ViewColor.layer.backgroundColor = .init(red: currentRed, green: currentGreen, blue: currentBlue, alpha: 1)
+        viewColor.layer.cornerRadius = 20
+     
     }
     
-    @IBAction func redSliderAction() {
-        RedLabel.text = String(Int(RedSlider.value))
-        currentRed = CGFloat(RedSlider.value)
-        viewCurrent()
-    }
-    
-    @IBAction func blueSliderAction() {
-        BlueLabel.text = String(Int(BlueSlider.value))
-        currentBlue = CGFloat(BlueSlider.value)
-        viewCurrent()
-    }
-    
-    @IBAction func greenSliderAction() {
-        GreenLabel.text = String(Int(GreenSlider.value))
-        currentGreen = CGFloat(GreenSlider.value)
-        viewCurrent()
+    @IBAction func changesOfSladers() {
+        
     }
     
 }
