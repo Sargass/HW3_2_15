02ba5421f51d973a31b7c10105603a54bffdae84
Sargass <alexey.simochkin@gmail.com>
2021-09-17 16:28:12 +0300

    Final!!!

--- HW3_2_15/ViewController.swift
+++ HW3_2_15/ViewController.swift
@@ -30,17 +30,50 @@ class ViewController: UIViewController {
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         
+        setColorOnView()
         
+        redLabel.text = setStringForLabel(from: redSlider)
+        greenLabel.text = setStringForLabel(from: greenSlider)
+        blueLabel.text = setStringForLabel(from: blueSlider)
         
-    }
-
-    func viewCurrent(){
-        viewColor.layer.cornerRadius = 20
-     
+        setColorOnView()
+        
+        setValue(for: redLabel, greenLabel, blueLabel)
+        
     }
     
-    @IBAction func changesOfSladers() {
+    @IBAction func changesOfSladers(_ sender: UISlider) {
         
+        switch sender{
+        case redSlider: redLabel.text = setStringForLabel(from: sender)
+        case greenSlider: greenLabel.text = setStringForLabel(from: sender)
+        case blueSlider: blueLabel.text = setStringForLabel(from: sender)
+        default: break
+        }
+   
+        setColorOnView()
+    }
+    
+    private func setColorOnView(){
+        viewColor.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
+    
+    private func setStringForLabel(from slider: UISlider) -> String{
+        String(format: "%.2f", slider.value)
+    }
+    
+    private func setValue(for labels: UILabel...){
+        labels.forEach {label in
+            switch label.tag {
+            case 0: redLabel.text = setStringForLabel(from: redSlider)
+            case 1: greenLabel.text = setStringForLabel(from: greenSlider)
+            case 2: blueLabel.text = setStringForLabel(from: blueSlider)
+            default: break
+            }
+        }
     }
     
 }
